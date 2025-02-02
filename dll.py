d0f494126db970e5712c0106f31a57c9d59961e1
Neelkanth Kaushik <me.neelkanth@gmail.com>
2025-02-02 21:32:48 +0530

    DLL

--- dll.py
+++ dll.py
@@ -10,6 +10,16 @@ class DLL:
     
     def is_empty(self):
         return self.start is None
+    
+    def print(self):
+        if self.start is None:
+            return
+        else:
+            cursor = self.start
+            while cursor is not None:
+                print(cursor.item, end=' ')
+                cursor = cursor.next
+            return
 
     def insert_at_start(self, data):
         new_node = Node(data)
@@ -30,22 +40,59 @@ class DLL:
                     break
                 cursor = cursor.next
 
-    def print(self):
+    def insert_after(self, data, item):
+        new_node = Node(data)
         if self.start is None:
-            pass
+            new_node.prev = self.start
+            new_node.next = None
+            self.start = new_node
         else:
             cursor = self.start
             while cursor is not None:
-                print(cursor.item, end=' ')
+                if cursor.item == item:
+                    new_node.prev = cursor
+                    new_node.next = cursor.next
+                    cursor.next = new_node
+                    cursor.next.prev = new_node
+                    break
                 cursor = cursor.next
+    
+    def delete_first(self):
+        if self.start is None:
+            return
+        else:
+            delete_node = self.start
+            self.start = delete_node.next
+            delete_node.next.prev = self.start
+            return delete_node
+        
+    def delete_last(self):
+        if self.start is None:
+            return
+        elif self.start.next is None:
+            self.start = None
+            return
+        else:
+            cursor = self.start
+            while cursor.next.next is not None:  # Stop at second-last node
+                cursor = cursor.next
+            cursor.next = None
+            return
+            
 
 mylist = DLL()
 mylist.insert_at_start(1)
 mylist.insert_at_start(2)
 mylist.insert_at_start(3)
 
-mylist.print()
+#mylist.print()
 
 mylist.insert_at_last(4)
 mylist.insert_at_last(5)
+#mylist.print()
+
+mylist.insert_after(0, 1)
+mylist.delete_first()
+print("\n")
 mylist.print()
+print("\n")
